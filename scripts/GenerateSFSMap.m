da8892d5191ecfddf00f3697f24d29cbf765395a
danielc518 <farewell518@gmail.com>
2015-11-10 22:14:34 -0500

    Bug fixes + light source direction estimation

--- scripts/GenerateSFSMap.m
+++ scripts/GenerateSFSMap.m
@@ -6,7 +6,7 @@
 clc; clf;
 
 % Read comet image
-image = imread('20150427101235-7719c656-xl.png');
+image = imread('20150427100331-a25c4b91-xl.png');
 
 % Create depth map from SFS algorithm
 map = ZhengChellappaMap(image);
@@ -15,7 +15,6 @@ map = ZhengChellappaMap(image);
 depth_map = medfilt2(abs(map),[21 21]);
 
 % Draw 3D surface
-figure;
 h = surfl(depth_map);
 set(h,'ydata',flipud(get(h,'ydata')));
 set(h,'xdata',flipud(get(h,'xdata')));
--- src/ZhengChellappaMap.m
+++ src/ZhengChellappaMap.m
@@ -1,33 +1,42 @@
 function [ map ] = ZhengChellappaMap( image )
 % Creates a DEM from a set of asteroid/comet images using shape from
-% shading.
+% shading proposed by Zheng and Chellappa.
 %
 % Input
-%   image : A cell array of images to create DEM from.
+%   image : An image to create DEM from.
 % Output
-%   map    : The DEM from the images.
+%   map    : The DEM from given image
 
+% convert to gray image
 image = double(mat2gray(image));
 
-% get image size
-[img_height,img_width] = size(image);
-
 % set parameters
-num_of_iterations = 100;
+num_of_iterations = 10;
 p_q_delta = 0.0001;
 mu = 1;
-albedo = 0.12;
-light_direction = [0.01 0.0 1.0]; % TODO: Need light direction
+albedo = 0.05;
 
-% normalize image
-for i=1:img_height
-    for j=1:img_width
-        image(i,j) = min(image(i,j) / albedo, 1.0);
-    end
+% estimate light direction
+Mu1 = mean(image(:));
+Mu2 = mean(mean(image.^2));
+
+[I_x,I_y] = imgradientxy(image);
+
+I_xy = sqrt(I_x.^2 + I_y.^2);
+I_x = I_x ./ (I_xy + 0.0001);
+I_y = I_y ./ (I_xy + 0.0001);
+
+slant = acos((4*Mu1)/sqrt((6 *(pi^2)* Mu2) - (48 * (Mu1^2))));
+tilt = atan(mean(I_y(:)) / mean(I_x(:)));
+if tilt < 0
+    tilt = tilt + pi;
 end
+
+light_direction = [cos(tilt)*sin(slant) sin(tilt)*sin(slant) cos(slant)];
 
 % determine number of hierarchies
 num_of_hierarchical_levels = 1;
+[img_height,img_width] = size(image);
 min_dim = min(img_height, img_width);
 while min_dim > 64
     min_dim = min_dim / 2 + rem(min_dim, 2);
@@ -35,31 +44,19 @@ while min_dim > 64
 end
 
 % create hierarchies
-img_hierarchy{1} = image;
-
-p{1} = zeros(img_height,img_width);
-q{1} = zeros(img_height,img_width);
-Z{1} = zeros(img_height,img_width);
-
-scale = 0.5;
-
-for h=2:num_of_hierarchical_levels
-    scaled_image = imresize(img_hierarchy{1}, scale);
-
-    [new_height, new_width] = size(scaled_image);
+for h=1:num_of_hierarchical_levels
+    images{h} = imresize(image, 0.5^(h-1));
     
-    img_hierarchy{h} = scaled_image;
+    [height, width] = size(images{h});
     
-    p{h} = zeros(new_height,new_width);
-    q{h} = zeros(new_height,new_width);
-    Z{h} = zeros(new_height,new_width);
-    
-    scale = scale * 0.5;
+    p{h} = zeros(height,width);
+    q{h} = zeros(height,width);
+    Z{h} = zeros(height,width);
 end
 
+% start iteration for each hierarchical level
 for h=num_of_hierarchical_levels:-1:1
-    % partial derivatives in each direction
-    [I_x,I_y] = imgradientxy(img_hierarchy{h});
+    [I_x,I_y] = imgradientxy(images{h});
     [I_xx,~] = imgradientxy(I_x);
     [~,I_yy] = imgradientxy(I_y);
     
@@ -75,34 +72,35 @@ for h=num_of_hierarchical_levels:-1:1
     [Z_xx,~] = gradient(Z_x);
     [~,Z_yy] = gradient(Z_y);
     
-    [current_height, current_width] = size(img_hierarchy{h});
+    [current_height, current_width] = size(images{h});
     
     for t=1:num_of_iterations
-        R = (-light_direction(1).* p{h} - light_direction(2).* q{h} + light_direction(3))./sqrt(1 + p{h}.^2 + q{h}.^2);
+        R = albedo .* (-light_direction(1).* p{h} - light_direction(2).* q{h} + light_direction(3))./sqrt(1 + p{h}.^2 + q{h}.^2);
         R = max(0,R);
         
-        R_p = (-light_direction(1).* (p{h} + p_q_delta) - light_direction(2).* q{h} + light_direction(3))./sqrt(1 + (p{h} + p_q_delta).^2 + q{h}.^2);
+        R_p = albedo .* (-light_direction(1).* (p{h} + p_q_delta) - light_direction(2).* q{h} + light_direction(3))./sqrt(1 + (p{h} + p_q_delta).^2 + q{h}.^2);
         R_p = max(0,R_p);
         R_p = (R_p - R) ./ p_q_delta;
         
-        R_q = (-light_direction(1).* p{h} - light_direction(2).* (q{h} + p_q_delta) + light_direction(3))./sqrt(1 + p{h}.^2 + (q{h} + p_q_delta).^2);
+        R_q = albedo .* (-light_direction(1).* p{h} - light_direction(2).* (q{h} + p_q_delta) + light_direction(3))./sqrt(1 + p{h}.^2 + (q{h} + p_q_delta).^2);
         R_q = max(0,R_q);
         R_q  = (R_q - R) ./ p_q_delta;
         
         A_11 = 5 .* R_p.^2 + 1.25 .* mu;
         A_12 = 5 .* R_p .* R_q + 0.25 .* mu;
         A_22 = 5 .* R_q.^2 + 1.25 .* mu;
-        A_delta = A_11 .* A_22 - A_12.^2;
         
-        lunate_eps = R - img_hierarchy{h};
+        lunate_eps = R - images{h};
         lower_eps = R_p .* (p_xx + p_yy) + R_q .* (q_xx + q_yy) - I_xx - I_yy;
         
         C_3 = -p_x - q_y + Z_xx + Z_yy;
         C_1 = (lower_eps - lunate_eps) .* R_p - mu .* (p{h} - Z_x) - 0.25 .* mu .* C_3;
         C_2 = (lower_eps - lunate_eps) .* R_q - mu .* (q{h} - Z_y) - 0.25 .* mu .* C_3;
         
-        d_p = (C_1 .* A_22 - C_2 .* A_12) ./ A_delta;
-        d_q = (C_2 .* A_11 - C_1 .* A_12) ./ A_delta;
+        determinant = A_11 .* A_22 - A_12.^2;
+        
+        d_p = (C_1 .* A_22 - C_2 .* A_12) ./ determinant;
+        d_q = (C_2 .* A_11 - C_1 .* A_12) ./ determinant;
         d_Z = (C_3 + d_p + d_q) ./ 4;
         
         for i=1:current_height
@@ -120,32 +118,32 @@ for h=num_of_hierarchical_levels:-1:1
         break
     end
     
-    [height, width] = size(img_hierarchy{h-1});
+    [height, width] = size(images{h-1});
     
     for i=1:height
         for j=1:width
-            i_prime = round(i/2);
-            j_prime = round(j/2);
+            i_half = round(i/2);
+            j_half = round(j/2);
             
             if rem(i,2) == 1 && rem(j,2) == 1
-                p{h-1}(i,j) = p{h}(i_prime,j_prime);
-                q{h-1}(i,j) = q{h}(i_prime,j_prime);
-                Z{h-1}(i,j) = Z{h}(i_prime,j_prime);
+                p{h-1}(i,j) = p{h}(i_half,j_half);
+                q{h-1}(i,j) = q{h}(i_half,j_half);
+                Z{h-1}(i,j) = Z{h}(i_half,j_half);
                 
-                if i_prime <= current_height && j_prime + 1 <= current_width
-                    p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_prime,j_prime + 1);
-                    q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_prime,j_prime + 1);
-                    Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_prime,j_prime + 1);
+                if i_half <= current_height && j_half + 1 <= current_width
+                    p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_half,j_half + 1);
+                    q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_half,j_half + 1);
+                    Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_half,j_half + 1);
                 end
-                if i_prime + 1 <= current_height && j_prime <= current_width
-                    p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_prime + 1,j_prime);
-                    q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_prime + 1,j_prime);
-                    Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_prime + 1,j_prime);
+                if i_half + 1 <= current_height && j_half <= current_width
+                    p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_half + 1,j_half);
+                    q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_half + 1,j_half);
+                    Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_half + 1,j_half);
                 end
-                if i_prime + 1 <= current_height && j_prime + 1 <= current_width
-                    p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_prime + 1,j_prime + 1);
-                    q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_prime + 1,j_prime + 1);
-                    Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_prime + 1,j_prime + 1);
+                if i_half + 1 <= current_height && j_half + 1 <= current_width
+                    p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_half + 1,j_half + 1);
+                    q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_half + 1,j_half + 1);
+                    Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_half + 1,j_half + 1);
                 end
                 
                 p{h-1}(i,j) = p{h-1}(i,j) * 0.25;
@@ -153,36 +151,36 @@ for h=num_of_hierarchical_levels:-1:1
                 Z{h-1}(i,j) = Z{h-1}(i,j) * 0.5;
             else
                 if rem(j,2) == 1
-                    p{h-1}(i,j) = p{h}(i_prime,j_prime);
-                    q{h-1}(i,j) = q{h}(i_prime,j_prime);
-                    Z{h-1}(i,j) = Z{h}(i_prime,j_prime);
+                    p{h-1}(i,j) = p{h}(i_half,j_half);
+                    q{h-1}(i,j) = q{h}(i_half,j_half);
+                    Z{h-1}(i,j) = Z{h}(i_half,j_half);
                     
-                    if i_prime <= current_height && j_prime + 1 <= current_width
-                        p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_prime,j_prime + 1);
-                        q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_prime,j_prime + 1);
-                        Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_prime,j_prime + 1);
+                    if i_half <= current_height && j_half + 1 <= current_width
+                        p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_half,j_half + 1);
+                        q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_half,j_half + 1);
+                        Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_half,j_half + 1);
                     end
                     
                     p{h-1}(i,j) = p{h-1}(i,j) * 0.5;
                     q{h-1}(i,j) = q{h-1}(i,j) * 0.5;
                 else
                     if rem(i,2) == 1
-                        p{h-1}(i,j) = p{h}(i_prime,j_prime);
-                        q{h-1}(i,j) = q{h}(i_prime,j_prime);
-                        Z{h-1}(i,j) = Z{h}(i_prime,j_prime);
+                        p{h-1}(i,j) = p{h}(i_half,j_half);
+                        q{h-1}(i,j) = q{h}(i_half,j_half);
+                        Z{h-1}(i,j) = Z{h}(i_half,j_half);
                         
-                        if i_prime + 1 <= current_height && j_prime <= current_width
-                            p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_prime + 1,j_prime);
-                            q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_prime + 1,j_prime);
-                            Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_prime + 1,j_prime);
+                        if i_half + 1 <= current_height && j_half <= current_width
+                            p{h-1}(i,j) = p{h-1}(i,j) + p{h}(i_half + 1,j_half);
+                            q{h-1}(i,j) = q{h-1}(i,j) + q{h}(i_half + 1,j_half);
+                            Z{h-1}(i,j) = Z{h-1}(i,j) + Z{h}(i_half + 1,j_half);
                         end
                         
                         p{h-1}(i,j) = p{h-1}(i,j) * 0.5;
                         q{h-1}(i,j) = q{h-1}(i,j) * 0.5;
                     else
-                        p{h-1}(i,j) = p{h}(i_prime,j_prime);
-                        q{h-1}(i,j) = q{h}(i_prime,j_prime);
-                        Z{h-1}(i,j) = Z{h}(i_prime,j_prime) * 2;
+                        p{h-1}(i,j) = p{h}(i_half,j_half);
+                        q{h-1}(i,j) = q{h}(i_half,j_half);
+                        Z{h-1}(i,j) = Z{h}(i_half,j_half) * 2;
                     end
                 end
             end
