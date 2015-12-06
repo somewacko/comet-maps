c96b85c6380f350ff040be168224bc6850d20c1d
danielc518 <farewell518@gmail.com>
2015-12-06 11:26:43 -0500

    Implemented Pentland SfS method

--- src/ShapeFromShading/PentlandMap.m
+++ src/ShapeFromShading/PentlandMap.m
@@ -1,33 +1,30 @@
 function [ map ] = PentlandMap( image )
 % Creates a DEM from a set of asteroid/comet images using shape from
-% shading proposed by Tsai and Shah.
-% Code from http://blog.tibarazmi.com/shape-shading-pentland-approach/
+% shading proposed by Pentland (1988).
 %
 % Input
 %   image : An image to create DEM from.
 % Output
 %   map    : The DEM from given image
 
-% making sure that it is a grayscale image
-E = mat2gray(image);
-E = double(E);
-% normalizing the image to have maximum of one
-E = E ./ max(E(:));
-%E = boost_shadow(E); imshow(E);
-% first compute the surface albedo and illumination direction
-[slant,tilt] = SlantTiltEstimation (E);
+I = double(mat2gray(image));
+I = I ./ max(I(:));
+
+[num_rows,num_cols] = size(I);
+
+[slant,tilt] = SlantTiltEstimation(I);
  
-% compute the fourier transform of the image
-Fe = fft2(E);
- 
-% wx and wy
-[M,N] = size(E);
-[x,y] = meshgrid(1:N,1:M);
-wx = (2.* pi .* x) ./ M;
-wy = (2.* pi .* y) ./ N;
- 
-% Using the estimated illumination direction
-Fz =Fe./(-1i.*wx.*cos(tilt).*sin(slant)-1i.*wy.*sin(tilt).*sin(slant));
- 
-% Compute the inverse Fourier transform to recover the surface.
-map = abs(ifft2(Fz));
+% Compute Fourier transform of the image
+F_I = fft2(I);
+
+% Compute Fourier transform of the depth map given FT of image
+F_Z = zeros(num_rows, num_cols);
+
+for i=1:num_rows
+    for j=1:num_cols
+        F_Z(i,j) = F_I(i,j) / (-1i * ((2 * pi * j / num_rows) * cos(tilt).*sin(slant) + (2 * pi * i / num_rows) * sin(tilt) * sin(slant)));
+    end
+end
+
+% Compute inverse Fourier transform to recover depth map
+map = abs(ifft2(F_Z));
