d8e0074b4713fd4904ad4ba20dba43f2abfa9253
danielc518 <farewell518@gmail.com>
2015-12-08 18:08:00 -0500

    Different implementation of Tsai and Shah SfS method

--- sfs/TsaiShahMap.m
+++ sfs/TsaiShahMap.m
@@ -1,59 +1,51 @@
 function [ map ] = TsaiShahMap( image )
 % Creates a DEM from a set of asteroid/comet images using shape from
-% shading proposed by Tsai and Shah.
-% Code from http://blog.tibarazmi.com/shape-shading-tsai-shah-approach/
+% shading proposed by Tsai and Shah (Shape From Shading Using Linear Approximation).
+% Link to paper: http://crcv.ucf.edu/papers/ivcj94.pdf
 %
 % Input
 %   image : An image to create DEM from.
 % Output
 %   map    : The DEM from given image
 
-% making sure that it is a grayscale image
-E = mat2gray(image);
-E = double(E);
-% first compute the surface albedo and illumination direction
-[slant,tilt] = SlantTiltEstimation(E);
+I = double(mat2gray(image));
  
-% initializations
-[M,N] = size(E);
-% surface normals
-p = zeros(M,N);
-q = zeros(M,N);
-% the surface
-Z = zeros(M,N);
-% surface derivatives in x and y directions
-Z_x = zeros(M,N);
-Z_y = zeros(M,N);
-% maximum number of iterations
-maxIter = 200;
-% the normalized illumination direction
-ix = cos(tilt) * tan(slant);
-iy = sin(tilt) * tan(slant);
+[num_rows,num_cols] = size(I);
+
+Z_n = zeros(num_rows,num_cols); % up-to-date depth values
+Z_n_1 = zeros(num_rows,num_cols); % depth values of previous iteration
+S = zeros(num_rows,num_cols) + 0.01;
+
+[slant,tilt] = SlantTiltEstimation(I);
+
+i_x = cos(tilt) * tan(slant);
+i_y = sin(tilt) * tan(slant);
+
+num_iterations = 200;
  
-for k = 1 : maxIter
-    % using the illumination direction and the currently estimate
-    % surface normals, compute the corresponding reflectance map.
-    R =(cos(slant) + p .* cos(tilt)*sin(slant)+ q .* ...
-        sin(tilt)*sin(slant))./sqrt(1 + p.^2 + q.^2);
-    % at each iteration, make sure that the reflectance map is positive at
-    % each pixel, set negative values to zero.
-    R = max(0,R);
-    % compute our function f which is the deviation of the computed
-    % reflectance map from the original image ...
-    f = E - R;
-    % compute the derivative of f with respect to our surface Z
-    df_dZ =(p+q).*(ix*p + iy*q + 1)./(sqrt((1 + p.^2 + q.^2).^3)* ...
-        sqrt(1 + ix^2 + iy^2))-(ix+iy)./(sqrt(1 + p.^2 + q.^2)* ...
-        sqrt(1 + ix^2 + iy^2));
-    % update our surface
-    Z = Z - f./(df_dZ + eps); % to avoid dividing by zero
-    % compute the surface derivatives with respect to x and y
-    Z_x(2:M,:) = Z(1:M-1,:);
-    Z_y(:,2:N) = Z(:,1:N-1);
-    % using the updated surface, compute new surface normals
-    p = Z - Z_x;
-    q = Z - Z_y;
+for iterations = 1:num_iterations
+    for i = 1:num_rows
+        for j = 1:num_cols
+            p = 0.0;
+            q = 0.0;
+            
+            if j - 1 >= 1 && i - 1 >= 1
+                p = Z_n_1(i,j) - Z_n_1(i, j-1);
+                q = Z_n_1(i,j) - Z_n_1(i-1, j);
+            end
+            
+            f = -(I(i,j) - max(0.0,(1+p*i_x+q*i_y)/(sqrt(1+p^2+q^2)*sqrt(1+i_x^2+i_y^2))));
+            df = -((i_x+i_y)/(sqrt(1+p^2+q^2)*sqrt(1+i_x^2+i_y^2))-(p+q)*(1.0+p*i_x+q*i_y)/(sqrt((1+p^2+q^2)^3)*sqrt(1+i_x^2+i_y^2)));
+            
+            K = S(i,j)*df/(eps+S(i,j)*df^2);
+            
+            S(i,j) = S(i,j) - S(i,j)*K*df;
+            
+            Z_n(i,j) = Z_n_1(i,j) + K*f;
+        end
+    end
+    
+    Z_n_1 = Z_n;
 end
- 
-% smoothing the recovered surface
-map = medfilt2(abs(Z),[21 21]);
+
+map = medfilt2(abs(Z_n),[21 21]);
